53900e713d269f5a4718fed2666f0df52a86894f
Carlos <carlosjcv94@gmail.com>
2023-08-11 20:24:56 +0200

    Still broken gotta fix

--- interview-frontend/src/app/city-search/city-search.component.ts
+++ interview-frontend/src/app/city-search/city-search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 interface City {
-  uuid: string;
+  uuid: string | number;
   name: string;
   count: number;
 }
@@ -13,22 +13,19 @@ interface City {
   styleUrls: ['./city-search.component.scss']
 })
 
-export class CitySearchComponent {
-  searchQuery = "";
-  cities: City[] = [
-    { uuid: "7e8a29e2-62d1-4ec1-ae15-8ff2f777318f", name: "Berlin", count: 523 },
-    { uuid: "4a7f5c2d-3a10-4a02-a9b3-450839929e43", name: "Hamburg", count: 267 },
-    { uuid: "09a20ce8-eb77-40f9-99c8-aa4e7dbf6a99", name: "München", count: 899 }
-  ]
-  searchResults: City[] = [];
+export class CitySearchComponent implements OnInit {
+  searchQuery: string = "";
+  cities: City[] = [];
+  currentPage: number = 1;
 
-  // constructor() {}
+  constructor(private http: HttpClient) {}
 
-  // ngOnInit(): void {
+  ngOnInit(): void {
 
-  // }
+  }
 
   searchCities(): void {
-    this.searchResults = this.cities.filter((city) => city.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+    this.currentPage = 1;
+    this.fetchCities();
   }
 }
